85853bda8e388895741c2dfa020f7c414ba6d64c
Dandy Arya Akbar <dandyarya003@gmail.com>
2024-11-23 12:14:47 +0700

    add_subcategories_support

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,14 +4,43 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'slug', 'image', 'is_active'];
+    protected $fillable = ['name', 'description', 'slug', 'image', 'is_active'];
 
     public function products() {
         return $this->hasMany(Product::class);
+    }
+
+    public function parent_category() {
+        return $this->belongsTo(Category::class, 'parent_category_id');
+    }
+
+    public function parent_category_recursive() {
+        return $this->parent_category()->with('parent_category_recursive');
+    }
+
+    public function parent_category_recursive_flatten() {
+        $result = collect();
+        $item = $this->parent_category_recursive;
+        if ($item instanceof User) {
+            $result->push($item);
+            $result = $result->merge($item->parent_category_recursive_flatten());
+        }
+        return $result;
+    }
+
+    public function children_category() {
+        return $this->hasMany(Category::class, 'parent_category_id');
+    }
+
+    public function children_category_recursive() {
+        return $this->children_category()->with('children_category_recursive');
     }
 }
---
+++ database/migrations/2024_11_22_054920_add_subcategory_column_to_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->string('description')->nullable();
+            $table->foreignId('parent_category_id')->nullable()->constrained('categories', 'id')->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropForeign(['parent_category_id']);
+            $table->dropColumn('parent_category_id');
+            $table->dropColumn('description');
+        });
+    }
+};
