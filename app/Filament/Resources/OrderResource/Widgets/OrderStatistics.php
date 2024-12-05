6c3d7fb02b3f10b8a8eb423f2e32d2b22305aac4
Dandy Arya Akbar <dandyarya003@gmail.com>
2024-12-05 08:36:04 +0700

    bug fix: line 20 on orderstatistics.php

--- app/Filament/Resources/OrderResource/Widgets/OrderStatistics.php
+++ app/Filament/Resources/OrderResource/Widgets/OrderStatistics.php
@@ -17,7 +17,7 @@ class OrderStatistics extends BaseWidget
             Stat::make('Order(s) Shipped', Order::query()->where('status', 'shipped')->count()),
             Stat::make('Order(s) Delivered', Order::query()->where('status', 'delivered')->count()),
             Stat::make('Order(s) Cancelled', Order::query()->where('status', 'cancelled')->count()),
-            Stat::make('Average Price', Number::currency(Order::query()->avg('grand_total'), 'IDR', 'id'))            
+            Stat::make('Average Price', Number::currency(Order::query()->avg('grand_total') ?? 0, 'IDR', 'id'))
         ];
     }
 }
