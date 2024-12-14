1bcb88248fb7f7fdd9aa1000b78311f2025d61b3
Dandy Arya Akbar <dandyarya003@gmail.com>
2024-12-14 14:18:23 +0700

    Progress

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagementDatabase;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+#[Title('ShopEasily™ - Check Out Your Cart')]
 class CheckoutPage extends Component
 {
     public function render()
     {
-        return view('livewire.checkout-page');
+        $cart_items = CartManagementDatabase::getCartItemsFromDatabase();
+        $grand_total = CartManagementDatabase::calculateGrandTotal();
+        return view('livewire.checkout-page', [
+            'cart_items' => $cart_items,
+            'grand_total' => $grand_total
+        ]);
     }
 }
