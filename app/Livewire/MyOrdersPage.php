4692b28d1620116874d1c0d9034045db3d4b660e
Dandy Arya Akbar <dandyarya003@gmail.com>
2024-12-26 14:39:08 +0700

    My Orders History and My Order Details Page DONE

--- app/Livewire/MyOrdersPage.php
+++ app/Livewire/MyOrdersPage.php
@@ -4,10 +4,38 @@ namespace App\Livewire;
 
 use Livewire\Component;
 
+#[Title('My Orders Page - ShopEasily™')]
 class MyOrdersPage extends Component
 {
+    use WithPagination;
+
+    public $payment_methods_map = [
+        'midtrans' => 'MidTrans Payment Gateway',
+        'cod' => 'Cash on Delivery (CoD)',
+    ];
+
+    public $order_status_to_color = [
+        'new' => 'bg-blue-500',
+        'processing' => 'bg-yellow-500',
+        'shipped' => 'bg-orange-500',
+        'delivered' => 'bg-green-500',
+        'cancelled' => 'bg-red-500',
+    ];
+
+    public $order_payment_status_to_color = [
+        'pending' => 'bg-yellow-500',
+        'paid' => 'bg-green-500',
+        'failed' => 'bg-red-500',
+    ];
+
     public function render()
     {
-        return view('livewire.my-orders-page');
+        $my_orders = Order::where('user_id', auth()->id())->latest()->paginate(5);
+        return view('livewire.my-orders-page',
+        ['orders'=> $my_orders,
+        'order_status_to_color_map' => $this->order_status_to_color,
+        'order_payment_status_to_color_map' => $this->order_payment_status_to_color,
+        'payment_methods_map' => $this->payment_methods_map]
+        );
     }
 }
