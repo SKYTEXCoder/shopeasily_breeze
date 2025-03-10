b43c9e21e6ab61b5c2bf3999083cc8edfdf6a46b
Dandy Arya Akbar <dandyarya003@gmail.com>
2025-03-11 00:02:15 +0700

    random commit

--- resources/views/livewire/partials/navbar.blade.php
+++ resources/views/livewire/partials/navbar.blade.php
@@ -67,7 +67,7 @@
                                         @endforeach
                                     </ul>
                                 </div>
-                                <div class="relative w-full md:w-[200px] lg:w-[300px]">
+                                <div class="relative w-full md:w-[200px] lg:w-[340px]">
                                     <input type="search" id="search-dropdown" name="searchQuery"
                                         class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
                                         placeholder="What kind of products are you looking for?" value="" required />
